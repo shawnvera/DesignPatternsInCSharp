2b5451f7af5e4213a84c1e9e9a77731305fa3579
Shawn <134296052+shawnvera@users.noreply.github.com>
2025-01-19 21:43:29 -0500

    update

--- DesignPatternsInCSharp/obj/Debug/net7.0/DesignPatternsInCSharp.AssemblyInfo.cs
+++ DesignPatternsInCSharp/obj/Debug/net7.0/DesignPatternsInCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatternsInCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9206d5c71463be4aa5914d574e567651e3624a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d765fa2d4cca9a5340c4b6ee93879553cac46f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatternsInCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatternsInCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

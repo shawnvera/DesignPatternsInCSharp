d47de2b65d7619abe9948d406aca055224f77e60
Shawn <134296052+shawnvera@users.noreply.github.com>
2025-01-19 23:16:04 -0500

    added adapter pattern practice

--- DesignPatternsInCSharp/Program.cs
+++ DesignPatternsInCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DesignPatternsInCSharp;
+using DesignPatternsInCSharp.AdapterDesignPattern;
 using DesignPatternsInCSharp.DependencyInjectionDesignPattern;
 using DesignPatternsInCSharp.FactoryDesignPattern;
 
@@ -11,3 +12,13 @@ product.StateName("This is from the factory design pattern.");
 // Dependency Injection Design Pattern
 Dog dog= new Dog("Girl");
 dog.MakeNoise();
+
+// Adapter Design Pattern
+// Round peg
+IRoundPeg roundPeg = new RoundPeg(5);
+Console.WriteLine($"RoundPeg radius: {roundPeg.GetRadius()}");
+
+// Square peg
+SquarePeg squarePeg = new SquarePeg(7);
+IRoundPeg adapter = new SquarePegAdapter(squarePeg);
+Console.WriteLine($"SquarePeg adapted radius: {adapter.GetRadius()}");
--- DesignPatternsInCSharp/obj/Debug/net7.0/DesignPatternsInCSharp.AssemblyInfo.cs
+++ DesignPatternsInCSharp/obj/Debug/net7.0/DesignPatternsInCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatternsInCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d765fa2d4cca9a5340c4b6ee93879553cac46f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5451f7af5e4213a84c1e9e9a77731305fa3579")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatternsInCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatternsInCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
